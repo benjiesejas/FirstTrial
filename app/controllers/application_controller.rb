856a1c9f217b1ac822f22d10a33cd60913c42ae3
benjiesejas <benjie.daud@gmail.com>
2013-08-14 11:52:23 +0800

    second commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,9 @@
 class ApplicationController < ActionController::Base
-  #protect_from_forgery
-  before_filter :set_account, :authenticate
-    protected
-    def set_account
-      @account = Account.find_by(url_name: request.subdomains.first)
-    end
+  helper_method :current_user
+  
+	private
 
-    def authenticate
-      case request.format
-      when Mime::XML, Mime::ATOM
-        if user = authenticate_with_http_basic { |u, p| @account.users.authenticate(u, p) }
-          @current_user = user
-        else
-          request_http_basic_authentication
-        end
-      else
-        if session_authenticated?
-          @current_user = @account.users.find(session[:authenticated][:user_id])
-        else
-          redirect_to(login_url) and return false
-        end
-      end
-    end
+	def current_user
+	  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Blog::Application.routes.draw do
 
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  #get "posts" => "posts#index", :as => "posts"
+
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+
+  resources :users
+  resources :sessions
+
   resources :posts do
 	resources :comments
   end
